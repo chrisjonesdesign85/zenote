845e6d1884db63600cb0906a6d8ce3cb9da00056
chrisJones <chrisjonesdesign85@gmail.com>
2023-01-23 23:20:21 -0500

    got the update button working with target div, and content goes into the modal

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ let span = document.getElementsByClassName("close")[0];
 // get the post submit button
 let postSubmit = document.getElementsByClassName("btn")
 
+// form title
+let formTitleValue = document.forms["myForm"]["title"]
+//form subject
+let subjectValue = document.forms["myForm"]["subject"]
+// form textarea
+let boxValue = document.forms["myForm"]["textarea"]
+
+
+
 //When the user clicks the button, open the modal
 newPostBtn.onclick = () => {
     modal.style.display = "block";
@@ -30,7 +39,7 @@ span.onclick = () => {
 //     }
 // }
 
-test = () => {
+addNote = () => {
     // get the input value
     let inputValue = document.querySelector("#myModal > div > div > form > fieldse > label:nth-child(2) > input").value
 
@@ -77,6 +86,9 @@ test = () => {
     editIcon.classList.add("fas")
     editIcon.classList.add("fa-edit")
 
+    // add the onClick attribute to the edit Icon
+    editIcon.setAttribute("onClick", "updatePost(this)")
+
     // create delete icon
     let deleteIcon = document.createElement("i")
 
@@ -84,6 +96,7 @@ test = () => {
     deleteIcon.classList.add("fas")
     deleteIcon.classList.add("fa-trash-alt")
 
+    // add the onClick attribute to the delete Icon
     deleteIcon.setAttribute("onClick", "deletePost(this)")
 
     // add the icons to the options div
@@ -92,8 +105,6 @@ test = () => {
 
     // add the options div to the titleDiv
     titleDiv.appendChild(optionsDiv)
-
-
 
     // create a paragraph element
     let p = document.createElement("p")
@@ -104,41 +115,50 @@ test = () => {
     // add the p the the new post div
     div.appendChild(p)
 
-    // Create post-info section
     // assign date to a variable
     let date = new Date().toDateString()
-    // create a div
+
+    // create a post-info div
     postInfo = document.createElement("div")
 
     //create a p element
     postInfoP = document.createElement("p")
+
     //add class name of post-info to the div
     postInfoP.classList.add('post-info')
+
     // Create post-info data
     let info = `<p>Created on <span class="date">${date}</span> by<span class="author"> ${username}</span`;
+
     // update the post info paragrah with the date and username
     postInfoP.innerHTML = info
+
     // postInfo.appendChild(infoData)
     div.appendChild(postInfoP)
+
     // get the container div
     let container = document.querySelector("body > div.container")
+
     // add the div with all the data into the container div
     container.appendChild(div)
 
-    // post-submit
-
-    let yesValue = document.forms["myForm"]["title"]
-    let subjectValue = document.forms["myForm"]["subject"]
-    let boxValue = document.forms["myForm"]["textarea"]
-    yesValue.value = ""
+    // clear the forms
+    formTitleValue.value = ""
     subjectValue.value = ""
     boxValue.value = ""
 
+    // close the modal
     modal.style.display = "none";
 }
 
 
 // update post
+let updatePost = (e) => {
+    deletePost(e)
+    modal.style.display = "block";
+    formTitleValue.value = e.parentElement.parentElement.textContent
+    boxValue.value = e.parentElement.parentElement.nextSibling.textContent
+}
 
 // delete post
 let deletePost = (e) => {
